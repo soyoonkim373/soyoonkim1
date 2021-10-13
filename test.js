b444730922517b5b6742063e896992634bd1cab4
soyoonkim373 <92446248+soyoonkim373@users.noreply.github.com>
2021-10-13 22:36:19 +0900

    first version

---
+++ test.js
@@ -0,0 +1,27 @@
+var star = '';
+for (var i = 1; i >= 5; i++) {
+
+  for (var j = 1; j <= i; j++) {
+    star += '*';
+  }
+  star += '\n';
+}
+  console.log(star)
+
+
+
+// if (i==1) {
+//     console.log('*')
+// }
+// if (i==2) {
+//     console.log('**')
+// }
+// if (i==3) {
+//     console.log('***')
+// }
+// if (i==4) {
+//     console.log('****')
+// }
+// if (i==5) {
+//     console.log('*****')
+// }
